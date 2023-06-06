fa22b09bf02939f3e03a55ff7e531403b7398dd4
Whatsuf148 <whatsufsachin@gmail.com>
2023-06-06 14:58:30 +0545

    Bug Fixed

--- lib/Screen/showburger.dart
+++ lib/Screen/showburger.dart
@@ -1,6 +1,5 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class ShowBurger extends StatelessWidget {
   const ShowBurger({super.key});
@@ -10,10 +9,48 @@ class ShowBurger extends StatelessWidget {
     return Scaffold(
         body: Stack(
       children: [
-        Container(
+        SizedBox(
           height: (MediaQuery.of(context).size.height),
+          width: (MediaQuery.of(context).size.width),
         ),
         Image.asset("photo/mithoburger.png"),
+        Positioned(
+            bottom: 0,
+            child: Container(
+              height: 390,
+              width: (MediaQuery.of(context).size.width),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 6),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Veg Burger",
+                          style: GoogleFonts.dmSans(
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w700,
+                            fontSize: 24,
+                          ),
+                        ),
+                       const Text("⭐4.8(Review)"),
+                      ],
+                    ),
+                    
+                  ),
+                ],
+              ),
+            ))
       ],
     ));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:cake_shop/Screen/homepage.dart';
 import 'package:flutter/material.dart';
 
+
 void main() {
   runApp(const MyApp());
 }
